5a3e4ff22069e804f7476729fd2b1fff2b8f7558
double2T <trinhvidanhthanh@gmail.com>
2026-06-08 23:30:52 +0700

    loi nho

--- src/config/payos.config.js
+++ src/config/payos.config.js
@@ -1,10 +1,15 @@
 const { PayOS } = require("@payos/node");
+
 const {
   PAYOS_CLIENT_ID,
   PAYOS_API_KEY,
   PAYOS_CHECKSUM_KEY,
 } = require("../constants/app.constant");
 
-const payOS = new PayOS(PAYOS_CLIENT_ID, PAYOS_API_KEY, PAYOS_CHECKSUM_KEY);
+const payOS = new PayOS({
+  clientId: PAYOS_CLIENT_ID,
+  apiKey: PAYOS_API_KEY,
+  checksumKey: PAYOS_CHECKSUM_KEY,
+});
 
 module.exports = payOS;
